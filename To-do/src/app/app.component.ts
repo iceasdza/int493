7b3eb754108f54ac7da8d461ea6374d457c39878
iceasdza <58130500056@st.sit.kmutt.ac.th>
2017-08-25 17:17:33 +0700

    todo

--- To-do/src/app/app.component.ts
+++ To-do/src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  todos:Todo[]= [
+    {id:1,task:"Go shopping",status:'Open'},
+    {id:2,task:"Do homework",status:'Open'}
+  ];
+  // newTask:string;
+  
+  addTask(newTask:string){
+       this.todos.push({id:this.todos.length+1,task:newTask,status:'Open'});   
+  }
+
+
+  doneTask(todo:Todo){
+  todo.status = 'Done';
+  }
+
+}
+
+export class Todo {
+  id:number;
+  task:string;
+  status:string = 'open';
+
+
+
 }
